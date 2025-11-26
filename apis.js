e4ae18f32f8ac81d4d1ef79b13452dcfd9febefa
PixelCrew Team <pixelcrewteam@gmail.com>
2025-11-26 18:44:27 -0400

    Crear apis.js

---
+++ apis.js
@@ -0,0 +1,8 @@
+global.APIs = {
+xyro: { url: "https://xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null }
+}
